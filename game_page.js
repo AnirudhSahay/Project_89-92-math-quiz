9608966bc9866a5b3725055afa13931da5fb2a6e
AnirudhSahay <101455843+AnirudhSahay@users.noreply.github.com>
2023-04-22 13:05:26 +0200

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -14,3 +14,22 @@ document.getElementById("player_answer").innerHTML="answer turn - " + Player2_na
 
 question_turn="player1";
 answer_turn="player2";
+
+function send(){
+number1=document.getElementById("number1").value;
+console.log(number1);
+number2=document.getElementById("number2").value;
+console.log(number2);
+actual_answer=parseInt(number1) * parseInt(number2)
+console.log(actual_answer);
+
+question_number = "<h4>" + number1 + " X " + number2 + "</h4>";
+input_box= "<br> Answer: <input type='text' id='input-check-box'>";
+check_buttton= "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+row= question_number + input_box + check_buttton;
+
+document.getElementById("output").innerHTML=row;
+
+document.getElementById("number1").value="";
+document.getElementById("number2").value="";
+}
